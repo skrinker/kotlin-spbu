cf9bcb5b47ddddcb3587f5e57242dfc65e5bb722
skrinker <guzel28sit@gmail.com>
2021-02-27 15:42:10 +0300

    fix null safety

--- src/main/kotlin/homework_1/task_1.1.kt
+++ src/main/kotlin/homework_1/task_1.1.kt
@@ -1,18 +1,33 @@
-import java.util.Scanner
+import java.lang.Integer.parseInt
+import java.lang.NumberFormatException
 
-fun factorialRec(number: Int): Int =  if (number < 1) 1 else number * factorialRec(number - 1);
+fun factorialRec(number: Int): Int {
+    if (number < 1)
+        return 1
+    else
+        return number * factorialRec(number - 1)
+}
 
-fun factorialLoop(number: Int) : Int {
-    var result = 1;
+fun factorialLoop(number: Int): Int {
+    var result = 1
     for (i in 1..number) result *= i
     return result
 }
 
-fun main() {
-    val reader = Scanner(System.`in`)
-    print("Enter a number: ")
-    var number:Int = reader.nextInt()
+fun getInputNumber(): Int? {
+    println("Enter a number: ")
+    val input = readLine()
+    return try {
+        parseInt(input)
+    } catch (e: NumberFormatException) {
+        null
+    }
+}
 
-    println(factorialRec(number))
-    println(factorialLoop(number))
+fun main() {
+    val number: Int = getInputNumber() ?: -1
+    if (number > -1) {
+        println(factorialRec(number))
+        println(factorialLoop(number))
+    }
 }
