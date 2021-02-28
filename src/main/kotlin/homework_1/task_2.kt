aa37f5c88a9dad3ede7bc40ab136b3461ced0417
Biktagirov Bulat <guzel28sit@gmail.com>
2021-02-28 16:00:09 +0300

    count matches using indexof

---
+++ src/main/kotlin/homework_1/task_2.kt
@@ -0,0 +1,27 @@
+package homework_1
+
+fun getStringInput(inputSuggestion: String): String {
+    println(inputSuggestion)
+    return readLine().toString()
+}
+
+fun countMatches(text: String, substring: String): Int {
+    var index = 0
+    var count = 0
+    while (true) {
+        index = text.indexOf(substring, index)
+        if (index != -1) {
+            count++
+            index += substring.length
+        } else {
+            return count
+        }
+    }
+}
+
+fun main() {
+    val text = getStringInput("Input text:")
+    val substring = getStringInput("Input string to find:")
+    val matches = countMatches(text, substring)
+    println(matches)
+}
