f69ee18c6deb1a30b8932e7e678238d187b0b4c5
Biktagirov Bulat <guzel28sit@gmail.com>
2021-03-11 20:04:02 +0300

    merged

--- src/main/kotlin/performedCommandStorage/Action.kt
+++ src/main/kotlin/performedCommandStorage/Action.kt
@@ -36,13 +36,6 @@ class InsertBack(private val value: Int) : Action {
      *
      * @param numbers List of the numbers.
      */
-interface Action {
-    fun execute(numbers: MutableList<Int>)
-    fun undo(numbers: MutableList<Int>)
-}
-
-class InsertBack(private val value: Int) : Action {
-    override fun execute(numbers: MutableList<Int>) = numbers.add(numbers.size, value)
     override fun undo(numbers: MutableList<Int>) {
         numbers.removeLast()
     }
@@ -65,60 +58,34 @@ class Push(private val value: Int) : Action {
      *
      * @param numbers List of the numbers.
      */
-class Push(private val value: Int) : Action {
-    override fun execute(numbers: MutableList<Int>) = numbers.add(0, value)
     override fun undo(numbers: MutableList<Int>) {
         numbers.removeFirst()
     }
 }
-
-/**
- * This class supports swapping two elements in a list
- *
- * @param to Position of the first element.
- * @param from Position of the second element.
- */
-class Rearrange(private val to: Int, private val from: Int) : Action {
-    /**
-     * This function checks for the current index within the array.
-     *
-     * @param index Current index.
-     */
-    private fun MutableList<Int>.containsIndex(index: Int): Boolean = (index >= 0 && index < this.size)
-    /**
-     * This function swap two elements in a list.
-     *
-     * @param to Position of the first element.
-     * @param from Position of the second element.
-     */
-    private fun replace(to: Int, from: Int, numbers: MutableList<Int>) {
-        if (numbers.containsIndex(to) && numbers.containsIndex(from)) {
-            val fromValue = numbers[from]
-            numbers[from] = numbers[to]
-            numbers[to] = fromValue
-        }
-    }
-    /**
-     * This function execute swapping two elements in a list.
-     *
-     * @param numbers List of the numbers.
-     */
-    override fun execute(numbers: MutableList<Int>) = replace(to, from, numbers)
-    /**
-     * This function undo swapping two elements in a list.
-     *
-     * @param numbers List of the numbers.
-     */
-
 class Rearrange(private val to: Int, private val from: Int) : Action {
     private fun MutableList<Int>.containsIndex(index: Int): Boolean = (index >= 0 && index < this.size)
+    /**
+     * This function supports changing element position.
+     *
+     * @param to New position.
+     * @param from Previous position.
+     */
     private fun replace(to: Int, from: Int, numbers: MutableList<Int>) {
         if (numbers.containsIndex(to) && numbers.containsIndex(from)) {
             val temp = numbers.removeAt(from)
             numbers.add(to, temp)
         }
     }
-
+    /**
+     * This function execute changing element position.
+     *
+     * @param numbers List of the numbers.
+     */
     override fun execute(numbers: MutableList<Int>) = replace(to, from, numbers)
+    /**
+     * This function undo changing element position.
+     *
+     * @param numbers List of the numbers.
+     */
     override fun undo(numbers: MutableList<Int>) = replace(to, from, numbers)
 }
