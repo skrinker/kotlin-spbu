ac1968b0859eebbb1ed929e7d79847836e86ca9a
Biktagirov Bulat <guzel28sit@gmail.com>
2021-04-07 14:16:56 +0300

    added tempdir

--- src/test/kotlin/performedCommandStorage/PerformedCommandStorageTest.kt
+++ src/test/kotlin/performedCommandStorage/PerformedCommandStorageTest.kt
@@ -1,50 +1,64 @@
 package performedCommandStorage
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
 import java.io.File
-import kotlin.test.assertEquals
+import java.nio.file.Path
 
 internal class PerformedCommandStorageTest {
-    private val commandStorage = PerformedCommandStorage()
-    private val numbers = mutableListOf<Int>()
-    private val resources = "src/test/resources/homework2"
 
     @Test
-    fun testPerformedCommandStorage() {
+    fun testExecute() {
+        val commandStorage = PerformedCommandStorage()
+        val numbers = mutableListOf<Int>()
         commandStorage.executeOperation(Push(0), numbers)
-        commandStorage.executeOperation(Push(1), numbers)
-        commandStorage.executeOperation(InsertBack(2), numbers)
-        commandStorage.executeOperation(InsertBack(3), numbers)
+        commandStorage.executeOperation(InsertBack(1), numbers)
         commandStorage.executeOperation(Rearrange(0, 1), numbers)
-        commandStorage.executeOperation(Rearrange(0, 5), numbers)
-        assertEquals(mutableListOf(0, 1, 2, 3), numbers)
-        commandStorage.undoOperation(numbers)
-        assertEquals(mutableListOf(0, 1, 2, 3), numbers)
-        commandStorage.undoOperation(numbers)
-        assertEquals(mutableListOf(1, 0, 2, 3), numbers)
-        commandStorage.undoOperation(numbers)
-        assertEquals(mutableListOf(1, 0, 2), numbers)
-        commandStorage.undoOperation(numbers)
         assertEquals(mutableListOf(1, 0), numbers)
+    }
+
+    @Test
+    fun testUndo() {
+        val commandStorage = PerformedCommandStorage()
+        val numbers = mutableListOf<Int>()
+        commandStorage.executeOperation(Push(0), numbers)
+        commandStorage.executeOperation(InsertBack(1), numbers)
+        commandStorage.executeOperation(Rearrange(0, 1), numbers)
         commandStorage.undoOperation(numbers)
-        assertEquals(mutableListOf(0), numbers)
+        commandStorage.undoOperation(numbers)
+        commandStorage.undoOperation(numbers)
+        assertEquals(mutableListOf<Int>(), numbers)
+    }
+
+    @Test
+    fun testUndoOnEmptyStorage() {
+        val commandStorage = PerformedCommandStorage()
+        val numbers = mutableListOf<Int>()
+        commandStorage.undoOperation(numbers)
+        assertEquals(mutableListOf<Int>(), numbers)
     }
 
     @Test
     fun readFromJson() {
-        commandStorage.readFromJson("$resources/inputTest.json", numbers)
+        val commandStorage = PerformedCommandStorage()
+        val numbers = mutableListOf<Int>()
+        commandStorage.readFromJson(javaClass.getResource("/homework2/testFile.json").path, numbers)
         assertEquals(mutableListOf(0, 2, 1, 1), numbers)
     }
 
     @Test
-    fun serialize() {
+    fun serialize(@TempDir tempDir: Path) {
+        val commandStorage = PerformedCommandStorage()
+        val numbers = mutableListOf<Int>()
+        val temp = tempDir.resolve("testFile.json").toString()
         commandStorage.executeOperation(InsertBack(0), numbers)
         commandStorage.executeOperation(InsertBack(1), numbers)
         commandStorage.executeOperation(Push(2), numbers)
         commandStorage.executeOperation(Push(1), numbers)
         commandStorage.executeOperation(Rearrange(0, 2), numbers)
         commandStorage.executeOperation(Rearrange(1, 2), numbers)
-        commandStorage.serialize("$resources/testFile.json", false)
-        assertEquals(File("$resources/testFile.json").readText(), File("$resources/outputTest.json").readText())
+        commandStorage.serialize(temp, false)
+        assertEquals(javaClass.getResource("/homework2/testFile.json").readText(), File(temp).readText())
     }
 }
--- src/test/kotlin/performedCommandStorage/RearrangeTest.kt
+++ src/test/kotlin/performedCommandStorage/RearrangeTest.kt
@@ -1,31 +1,35 @@
 package performedCommandStorage
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
 internal class RearrangeTest {
 
     @Test
-    fun execute() {
+    fun testExecuteInsideIndex() {
         val numbers = mutableListOf(0, 1, 2)
-        // Index inside
-        Rearrange(0, 1).execute(numbers) 
-        assertEquals(mutableListOf(1, 0, 2), numbers)
-
-        // Index outside
-        Rearrange(0, 3).execute(numbers) 
+        Rearrange(0, 1).execute(numbers)
         assertEquals(mutableListOf(1, 0, 2), numbers)
     }
 
     @Test
-    fun undo() {
+    fun testExecuteOutsideIndex() {
         val numbers = mutableListOf(0, 1, 2)
-        // Index inside
+        Rearrange(0, 3).execute(numbers)
+        assertEquals(mutableListOf(0, 1, 2), numbers)
+    }
+		
+    @Test
+    fun testUndoInsideIndex() {
+        val numbers = mutableListOf(0, 1, 2)
         Rearrange(0, 1).undo(numbers)
         assertEquals(mutableListOf(1, 0, 2), numbers)
+    }
 
-        // Index outside
+    @Test
+    fun testUndoOutsideIndex() {
+        val numbers = mutableListOf(0, 1, 2)
         Rearrange(0, 3).undo(numbers)
-        assertEquals(mutableListOf(1, 0, 2), numbers)
+        assertEquals(mutableListOf(0, 1, 2), numbers)
     }
 }
