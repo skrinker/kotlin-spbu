9f46c59ef983013c626f4fe0a4b3b8724eeb104c
skrinker <guzel28sit@gmail.com>
2021-10-25 12:04:40 +0300

    fix requested changes

--- src/test/kotlin/homework4/avltree/TreeTest.kt
+++ src/test/kotlin/homework4/avltree/TreeTest.kt
@@ -1,8 +1,7 @@
 package homework4.avltree
 
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
